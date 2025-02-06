b88b02ba33a8c139a9cdeb80f760cbf9ffe88b1f
Paulo Rohling <paulohr.freecopias@gmail.com>
2025-02-05 23:20:42 -0300

    feat: adding created_by constraints to uuid v7

--- db/000003_note.up.sql
+++ db/000003_note.up.sql
@@ -6,5 +6,6 @@ CREATE TABLE "notes"
     created_at timestamptz  NOT NULL DEFAULT now(),
     created_by uuid         NOT NULL,
 
-    CONSTRAINT "uuid_notes_uuid_v7" CHECK (uuid_extract_version(id) = 7)
+    CONSTRAINT "uuid_notes_uuid_v7" CHECK (uuid_extract_version(id) = 7),
+    CONSTRAINT "created_by_notes_uuid_v7" CHECK (uuid_extract_version(created_by) = 7)
 );
